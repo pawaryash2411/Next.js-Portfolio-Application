c9d92a1cefb749f95b47fc3f4b3e818923289f57
pawaryas <yash.cs1993@mitindore.co.in>
2024-01-27 23:11:57 +0530

    jlnk

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -11,10 +11,6 @@ import { AiOutlineNumber } from "react-icons/ai";
 import { RiFacebookBoxFill } from "react-icons/ri";
 import Link from "next/link";
 
-const handleEmailClick = () => {
-  window.location.href = `mailto:${email}`;
-};
-
 const Footer = () => {
   return (
     <div className="w-full h-full bg-transparent text-gray-200 shadow-lg p-[15px] ">
